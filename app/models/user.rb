ab5c00778462e93713db2251ced62d03f25f49e5
tpayne84 <Travis.Payne84@me.com>
2013-09-29 12:04:44 -0500

    removed attr_accessible from the user model, in Rails 4 this goes against the strong attributes protection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
-
-  attr_accessible :email, :password, :password_confirmation
-
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
