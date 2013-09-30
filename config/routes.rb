c0c484b0b1c8ca3eb2e52a573792ed1bcfccab9e
tpayne84 <Travis.Payne84@me.com>
2013-09-29 19:04:12 -0500

    Update various styles - add post model with 50% styling complete on it, implemented devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Flatstrap::Application.routes.draw do
+  resources :posts
+
   devise_for :users
   root :to => 'static#index'
   # The priority is based upon order of creation: first created -> highest priority.
